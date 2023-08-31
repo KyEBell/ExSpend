94e8a56a5b1fb60970e4e2bc5bfccd4d56378d2a
KyEBell <kyleevanbell@gmail.com>
2023-08-31 19:44:56 -0400

    built out Expense component to modularize code away from App

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
-function ExpenseItem() {
+function ExpenseItem({ expense }) {
   return (
     <div className='expense-item'>
-      <div>August 30,2023</div>
+      <ExpenseDate date={expense.date} />
       <div className='expense-item__description'>
-        <h2>Car Insurance</h2>
-        <div className='expense-item__price'>$249</div>
+        <h2>{expense.title}</h2>
+        <div className='expense-item__price'>${expense.amount}</div>
       </div>
     </div>
   );
