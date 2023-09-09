c22477269ac42ed3f14601eb5fd6225736c0b8a4
KyEBell <kyleevanbell@gmail.com>
2023-09-09 10:30:29 -0400

    Year filter added, state lifted up to expenses.js

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,15 +1,27 @@
 import './Expenses.css';
+import { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 const Expenses = (props) => {
+  const [filterYear, setFilterYear] = useState('2021');
+  const handleFilterChange = (selectedYear) => {
+    setFilterYear(selectedYear);
+  };
   return (
-    <Card className='expenses'>
-      {props.expenses.map((element) => (
-        <div key={element.id}>
-          <ExpenseItem expense={element} />
-        </div>
-      ))}
-    </Card>
+    <div>
+      <Card className='expenses'>
+        <ExpensesFilter
+          onFilterChange={handleFilterChange}
+          selectedYear={filterYear}
+        />
+        {props.expenses.map((element) => (
+          <div key={element.id}>
+            <ExpenseItem expense={element} />
+          </div>
+        ))}
+      </Card>
+    </div>
   );
 };
 
