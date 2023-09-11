f36dddf1640a654284ceda1b8c3e2c5964ffc791
KyEBell <kyleevanbell@gmail.com>
2023-09-11 17:55:47 -0400

    added conditional rendering of the calendar filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,10 +4,25 @@ import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 const Expenses = (props) => {
-  const [filterYear, setFilterYear] = useState('2021');
+  const [filterYear, setFilterYear] = useState('Show All');
   const handleFilterChange = (selectedYear) => {
     setFilterYear(selectedYear);
   };
+
+  const filteredExpenses = props.expenses.filter((element) => {
+    if (filterYear !== 'Show All') {
+      return element.date.getFullYear().toString() === filterYear;
+    } else return true;
+  });
+  let expensesContent = <p>No Expenses Found</p>;
+  if (filteredExpenses.length > 0) {
+    expensesContent =
+      filteredExpenses.length > 0 &&
+      filteredExpenses.map((element) => (
+        <ExpenseItem key={element.id} expense={element} />
+      ));
+  }
+
   return (
     <div>
       <Card className='expenses'>
@@ -15,11 +30,7 @@ const Expenses = (props) => {
           onFilterChange={handleFilterChange}
           selectedYear={filterYear}
         />
-        {props.expenses.map((element) => (
-          <div key={element.id}>
-            <ExpenseItem expense={element} />
-          </div>
-        ))}
+        {expensesContent}
       </Card>
     </div>
   );
