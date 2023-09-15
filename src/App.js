205cc2919d33813eeda43031cd56c33b72851cf2
KyEBell <kyleevanbell@gmail.com>
2023-09-15 10:27:00 -0400

    updated color theme to blue shades

--- src/App.js
+++ src/App.js
@@ -5,21 +5,21 @@ import { useState } from 'react';
 const dummyExpenses = [
   {
     id: 'e1',
-    title: 'Toilet Paper',
-    amount: 94.12,
+    title: 'Paper Towels',
+    amount: 16.89,
     date: new Date(2020, 7, 14),
   },
   { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
   {
     id: 'e3',
     title: 'Car Insurance',
-    amount: 294.67,
+    amount: 185.62,
     date: new Date(2021, 2, 28),
   },
   {
-    id: 'e4', 
-    title: 'New Desk (Wooden)',
-    amount: 450,
+    id: 'e4',
+    title: 'Mousepad',
+    amount: 200.0,
     date: new Date(2021, 5, 12),
   },
 ];
